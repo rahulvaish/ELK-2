57e03e788d5387a688b763f5749ba190c7adea33
Rahul <vaish.rahul4810@gmail.com>
2018-07-08 01:06:23 +0530

    RESTCLIENT

--- RESTClientForElasticSearch/src/main/java/com/elastic/search/RESTClientForElasticSearch/RESTClientForElasticSearch.java
+++ RESTClientForElasticSearch/src/main/java/com/elastic/search/RESTClientForElasticSearch/RESTClientForElasticSearch.java
@@ -47,36 +47,7 @@ public class RESTClientForElasticSearch {
 	}
 	
 	
-	public static void postElasticData() {
-		try {
-
-			Client client = Client.create();
-			//CHANGE THE ID BEFORE HIT
-			WebResource webResource = client
-			   .resource("http://localhost:9200/crud/posts/005");
-
-			String input = "{\"singer\":\"HUTCH\",\"title\":\"ORANGE\"}";
-			
-
-			ClientResponse response = webResource.type("application/json")
-			   .post(ClientResponse.class, input);
-
-			if (response.getStatus() != 201) {
-				throw new RuntimeException("Failed : HTTP error code : "
-				     + response.getStatus());
-			}
-
-			System.out.println("Output from Server .... \n");
-			String output = response.getEntity(String.class);
-			System.out.println(output);
-
-		  } catch (Exception e) {
-
-			e.printStackTrace();
-
-		  }
-
-	}
+	
 	
 	public static void main(String args []) {
 		//CHANGE THE ID IN postElasticData() BEFORE HIT
